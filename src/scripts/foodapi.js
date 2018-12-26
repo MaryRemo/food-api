892ab63090a796e1aed8d5e6b8165d0c28e698dd
Mary Remo <maryjewel99@gmail.com>
2018-12-26 13:47:52 -0600

    second commit

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,5 +1,49 @@
 fetch("http://localhost:8088/food")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
-    })
+        parsedFoods.forEach(foodobj => {
+            console.log("food back from json server", foodobj);
+            // const foodAsHTML = foodFactory(foodobj);
+            // addFoodToDom(foodAsHTML);
+fetch(`https://world.openfoodfacts.org/api/v0/product/${foodobj.barcode}.json`)
+    .then(response => response.json())
+    .then(productInfo => {
+    console.log("productInfo ingredients", productInfo.product.ingredients_text);
+    foodobj.ingredients = productInfo.product.ingredients_text;
+    foodobj.country = productInfo.product.countries;
+    foodobj.sugar = productInfo.product.nutriments.sugar_value;
+    foodobj.fat = productInfo.product.nutriments.fat_value;
+    foodobj.calories = productInfo.product.nutriments.energy_serving;
+    console.log("foodObj with ingredients", foodobj);
+    const foodAsHTML = foodFactory (foodobj);
+    addFoodToDom(foodAsHTML);
+    });
+        });
+    });
+
+
+const foodFactory = (food) => {
+   let HTMLfoodSection = (`
+   <section class = "foodcard">
+       <h3> ${(food.name)}</h3>
+        <p>${(food.type)}</p>
+       <p>${(food.ethnicity)}</p>
+       <p>${(food.ingredients)}</p>
+       <p>${(food.sugar)}</p>
+       <p>${(food.fat)}</p>
+       <p>${(food.calories)}</p>
+       
+   </section> 
+`);
+console.log("html string", HTMLfoodSection);
+return HTMLfoodSection;
+};
+
+
+
+
+
+const addFoodToDom = (foodHTMl) => {
+  const articleContainer = document.querySelector(".foodList");
+  articleContainer.innerHTML += foodHTMl;
+};
